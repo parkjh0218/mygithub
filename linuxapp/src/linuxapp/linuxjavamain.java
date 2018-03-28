36b3777f6f06ec76f58de44986510cc6f3af7bf1
bit <bit@DESKTOP-3JEVMOV>
2018-03-28 15:16:01 +0900

    linux01

---
+++ linuxapp/src/linuxapp/linuxjavamain.java
@@ -0,0 +1,12 @@
+package linuxapp;
+
+public class linuxjavamain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			System.out.println("hello linux!");
+		
+	}
+
+}
