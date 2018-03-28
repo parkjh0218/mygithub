52f68db43a1c33e60859e177eb6714c53938ab54
bit <bit@DESKTOP-3JEVMOV>
2018-03-28 16:07:10 +0900

    t

---
+++ throwup/src/throwup/ThrowupMain.java
@@ -0,0 +1,12 @@
+package throwup;
+
+public class ThrowupMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("fighting!! never give up!!");
+		
+	}
+
+}
